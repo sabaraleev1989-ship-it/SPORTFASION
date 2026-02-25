3188380d18d4278fb52442bd119ce6ed84ccb424
sabaraleev1989-ship-it <sabaraleev1989@gmail.com>
2026-02-25 17:31:11 +0300

    Update products.js

--- products.js
+++ products.js
@@ -11,27 +11,27 @@ const products = [
         category: "Футболки",
         title: "Футболка Мужская Nike Black",
         newPrice: "2 500",
-        image: "nike_tee.png"
+        image: "2.png"
     },
     {
         id: 3,
         category: "Штаны",
         title: "Брюки Спортивные Adidas",
         newPrice: "4.800",
-        image: "pants.png"
+        image: "3.png"
     },
     {
         id: 4,
         category: "Рюкзаки",
         title: "Рюкзак Puma Городской",
         newPrice: "5 200",
-        image: "bag.png"
+        image: "4.png"
     },
     {
         id: 5,
         category: "Шапки",
         title: "Шапка теплая вязаная",
         newPrice: "1 200",
-        image: "hat.png"
+        image: "5.png"
     }
 ];
